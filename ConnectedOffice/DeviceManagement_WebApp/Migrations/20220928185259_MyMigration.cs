a353eb2aecda013e07c94577415bd8a07cbbe522
mapulemashinini <110629391+mapulemashinini@users.noreply.github.com>
2022-09-28 22:02:25 +0200

    Creating Repositories

---
+++ ConnectedOffice/DeviceManagement_WebApp/Migrations/20220928185259_MyMigration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeviceManagement_WebApp.Migrations
+{
+    public partial class MyMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    CategoryID = table.Column<Guid>(nullable: false),
+                    CategoryName = table.Column<string>(nullable: false),
+                    CategoryDescription = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.CategoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Zone",
+                columns: table => new
+                {
+                    ZoneID = table.Column<Guid>(nullable: false),
+                    ZoneName = table.Column<string>(nullable: false),
+                    ZoneDescription = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Zone", x => x.ZoneID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Device",
+                columns: table => new
+                {
+                    DeviceID = table.Column<Guid>(nullable: false),
+                    DeviceName = table.Column<string>(nullable: true),
+                    CategoryID = table.Column<Guid>(nullable: false),
+                    ZoneID = table.Column<Guid>(nullable: false),
+                    Status = table.Column<string>(nullable: true),
+                    IsActive = table.Column<bool>(nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime", nullable: false, defaultValueSql: "(getdate())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Device", x => x.DeviceID);
+                    table.ForeignKey(
+                        name: "FK_Device_Category",
+                        column: x => x.CategoryID,
+                        principalTable: "Category",
+                        principalColumn: "CategoryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Device_Zone",
+                        column: x => x.ZoneID,
+                        principalTable: "Zone",
+                        principalColumn: "ZoneID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_CategoryID",
+                table: "Device",
+                column: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Device_ZoneID",
+                table: "Device",
+                column: "ZoneID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Device");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "Zone");
+        }
+    }
+}
---
+++ ConnectedOffice/DeviceManagement_WebApp/Repository/CategoryRepository.cs
@@ -0,0 +1,25 @@
+﻿using DeviceManagement_WebApp.Data;
+using DeviceManagement_WebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DeviceManagement_WebApp.Repository
+{
+    public class CategoryRepository
+    {
+        protected readonly ConnectedOfficeContext _context = new ConnectedOfficeContext();
+
+        // GET ALL: Categories
+        public IEnumerable<Category> GetAll()
+        {
+            return (IEnumerable<Category>)_context.Category.ToList();
+        }
+
+        // TO DO: Add ‘Get By Id’
+        // TO DO: Add ‘Create’
+        // TO DO: Add ‘Edit’
+        // TO DO: Add ‘Delete’
+        // TO DO: Add ‘Exists’
+
+    }
+}
