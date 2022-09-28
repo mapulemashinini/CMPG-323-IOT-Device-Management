9ec2bebc696f1fb62d1c2ca8dcb6a6fbac7040a9
mapulemashinini <110629391+mapulemashinini@users.noreply.github.com>
2022-09-28 18:16:10 +0200

    Update Startup.cs
    allow user to login

--- ConnectedOffice/DeviceManagement_WebApp/Startup.cs
+++ ConnectedOffice/DeviceManagement_WebApp/Startup.cs
@@ -33,7 +33,7 @@ namespace DeviceManagement_WebApp
             services.AddDbContext<ConnectedOfficeContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
